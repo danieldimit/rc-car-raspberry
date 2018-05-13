ad325f6c8aad6424e0b7d222eab4f3c8259cc82d
danieldimit <dani2dim@yahoo.com>
2018-05-13 21:22:41 +0200

    Simplified program for the test

--- rcserver.py
+++ rcserver.py
@@ -16,23 +16,9 @@ GPIO.setmode(GPIO.BOARD)
 
 # set up GPIO pins
 GPIO.setup(7, GPIO.OUT) #Connected to PWMA
-GPIO.setup(11, GPIO.OUT) #Connected to AIN2
-GPIO.setup(12, GPIO.OUT) #Connected to AIN1
-GPIO.setup(13, GPIO.OUT) #Connected to STBY
-GPIO.setup(15, GPIO.OUT) #Connected to BIN1
-GPIO.setup(16, GPIO.OUT) #Connected to BIN2
-GPIO.setup(18, GPIO.OUT) #Connected to PWMB
-
-#Make sure STBY is disabled - Set it to HIGH
-GPIO.output(13, GPIO.HIGH)
 
 #Make sure the rest are disabled
-GPIO.output(7,GPIO.LOW)
-GPIO.output(11,GPIO.LOW)
-GPIO.output(12,GPIO.LOW)
-GPIO.output(15, GPIO.LOW)
-GPIO.output(16, GPIO.LOW)
-GPIO.output(18, GPIO.LOW)
+GPIO.output(7,GPIO.HIGH)
 
 #Bluetooth
 server_sock=BluetoothSocket( RFCOMM )
@@ -40,45 +26,28 @@ server_sock=BluetoothSocket( RFCOMM )
 #GPIO directions
 def forward():
     #Set the direction of Motor A
-    GPIO.output(12, GPIO.HIGH) #Set AIN1
-    GPIO.output(11, GPIO.LOW) #Set AIN2
-    #Set the Speed / PWM for A
-    GPIO.output(7, GPIO.HIGH) #Set PWMA
+    GPIO.output(7, GPIO.HIGH) #Set AIN1
     
 def backward():
     #Set the direction of Motor A
     GPIO.output(12, GPIO.LOW) #Set AIN1
     GPIO.output(11, GPIO.HIGH) #Set AIN2
-    #Set the Speed / PWM for A
-    GPIO.output(7, GPIO.HIGH) #Set PWMA
  
 def right():
     #Set the direction of Motor B
     GPIO.output(15, GPIO.HIGH) #Set BIN1
     GPIO.output(16, GPIO.LOW) #Set BIN2
-    #Set the Speed / PWM for B
-    GPIO.output(18, GPIO.HIGH) #Set PWMA
 
 def left():
     #Set the direction of Motor B
     GPIO.output(15, GPIO.LOW) #Set BIN1
     GPIO.output(16, GPIO.HIGH) #Set BIN2
-    #Set the Speed / PWM for B
-    GPIO.output(18, GPIO.HIGH) #Set PWMA
     
 def still_f_b():
-    #Now set everything to low (Switch everything Off)
-    GPIO.output(12, GPIO.LOW) #Set AIN1
-    GPIO.output(11, GPIO.LOW) #Set AIN2
-    GPIO.output(7, GPIO.LOW) #Set PWMA
-
+    GPIO.output(7, GPIO.HIGH) #Set PWMA
 
     
 def still_l_r():
-    #Now set everything to low (Switch everything Off)
-    GPIO.output(15, GPIO.LOW) #Set BIN1
-    GPIO.output(16, GPIO.LOW) #Set BIN2
-    GPIO.output(18, GPIO.LOW) #Set PWMA
 #end of direction functions
     
 server_sock.bind(("",PORT_ANY))
