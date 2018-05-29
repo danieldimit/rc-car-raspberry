05f2b8f3bc2f8a5868dac05e94b517e40cf1784f
danieldimit <dani2dim@yahoo.com>
2018-05-29 23:29:52 +0200

    Trying to fix connection bug

--- rcserver.py
+++ rcserver.py
@@ -66,14 +66,14 @@ port = server_sock.getsockname()[1]
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-while True:
-    advertise_service( server_sock, "SampleServer",
+advertise_service( server_sock, "SampleServer",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
                    profiles = [ SERIAL_PORT_PROFILE ], 
 #                   protocols = [ OBEX_UUID ] 
                     )
-                   
+
+while True:         
     print("Waiting for phone to engage connection %d" % port)
 
     client_sock, client_info = server_sock.accept()
