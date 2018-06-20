67821540dc02fb771018d74ce68b03e8329bc9f2
danieldimit <dani2dim@yahoo.com>
2018-06-21 00:44:49 +0200

    added loop, deployed on server, test if rspi gets commands

--- rcserver-web.py
+++ rcserver-web.py
@@ -1,13 +1,10 @@
-# file: rfcomm-server.py
-# auth: Albert Huang <albert@csail.mit.edu>
-# desc: simple demonstration of a server application that uses RFCOMM sockets
-#
-# $Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
 
 import RPi.GPIO as GPIO
 import time
 import urllib2
-contents = urllib2.urlopen("https://jsonplaceholder.typicode.com/posts/1").read()
 
-print contents
+
+while true:
+    contents = urllib2.urlopen("http://165.227.144.106:8080/getDirection").read()
+    print contents
 
